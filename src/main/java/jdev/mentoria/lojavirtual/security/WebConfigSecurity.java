98c7f7b31e39ac4f0739b8872f794e566881f753
Otavio <luizgomesdossantos1970@gmail.com>
2025-05-28 13:35:12 -0300

    Add new class of spring security.

---
+++ src/main/java/jdev/mentoria/lojavirtual/security/WebConfigSecurity.java
@@ -0,0 +1,15 @@
+package jdev.mentoria.lojavirtual.security;
+
+import javax.servlet.http.HttpSessionListener;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener {
+
+}
