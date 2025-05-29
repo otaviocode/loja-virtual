3c73287be77735696f82950e34229e610e5c42e3
Otavio <luizgomesdossantos1970@gmail.com>
2025-05-29 00:10:29 -0300

    I have added new settings and methods, and the initial method working.

--- src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
+++ src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
@@ -1,19 +1,71 @@
 package jdev.mentoria.lojavirtual;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import jdev.mentoria.lojavirtual.controller.AcessoController;
 import jdev.mentoria.lojavirtual.model.Acesso;
+import jdev.mentoria.lojavirtual.repository.AcessoRepository;
+import junit.framework.TestCase;
 
 @SpringBootTest(classes = LojaVirtualMentoriaApplication.class)
-class LojaVirtualMentoriaApplicationTests {
+class LojaVirtualMentoriaApplicationTests extends TestCase {
+
+	@Autowired
+	private AcessoController acessoController;
+
+	@Autowired
+	private AcessoRepository acessoRepository;
 
 	@Test
 	void contextLoads() {
 
+		/* Teste de criar acesso */
+
 		Acesso acesso = new Acesso();
 
-		acesso.setDescricao("ROLE_ADMIN");
+		acesso.setDescricao("ROLE_TEST_JUNIT");
+
+		assertEquals(true, acesso.getId() == null); /* Primeira camada de teste */
+
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+
+		assertEquals(true, acesso.getId() > 0); /* Segunda camada de teste */
+
+		assertEquals("ROLE_TEST_JUNIT", acesso.getDescricao()); /* Terceira camada de teste */
+
+		/* Teste de carregamento */
+
+		Acesso acesso2 = acessoRepository.findById(acesso.getId()).get();
+
+		assertEquals(acesso.getId(), acesso2.getId());
+
+		/* Teste de delete */
+
+		acessoRepository.deleteById(acesso2.getId());
+
+		acessoRepository.flush(); /* Roda esse SQL de delete no banco de dados */
+
+		Acesso acesso3 = acessoRepository.findById(acesso2.getId()).orElse(null);
+
+		assertEquals(true, acesso3 == null);
+
+		/* Teste de query */
+
+		acesso = new Acesso();
+
+		acesso.setDescricao("ROLE_ALUNO");
+
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+
+		List<Acesso> acessos = acessoRepository.buscarAcessoDesc("ALUNO".trim().toUpperCase());
+
+		assertEquals(1, acessos.size());
+
+		acessoRepository.deleteById(acesso.getId());
 
 	}
 
