22828f84615b82f95d67688825db961d62ff3318
Otavio <luizgomesdossantos1970@gmail.com>
2025-05-28 23:15:14 -0300

    Configuring new assets by controller and spring security.

--- src/main/java/jdev/mentoria/lojavirtual/controller/AcessoController.java
+++ src/main/java/jdev/mentoria/lojavirtual/controller/AcessoController.java
@@ -3,27 +3,33 @@ package jdev.mentoria.lojavirtual.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jdev.mentoria.lojavirtual.model.Acesso;
+//import jdev.mentoria.lojavirtual.repository.AcessoRepository;
 import jdev.mentoria.lojavirtual.service.AcessoService;
 
+@Controller
 @RestController
-@RequestMapping("/acessos")
 public class AcessoController {
 
 	@Autowired
 	private AcessoService acessoService;
+	/*
+	 * @Autowired private AcessoRepository acessoRepository;
+	 */
 
-	@PostMapping(value = "**/salvarAcesso")
-	@ResponseBody
-	public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) {
+	@ResponseBody /* Poder dar um retorno da API */
+	@PostMapping(value = "**/salvarAcesso") /* Mapeando a url para receber JSON */
+	public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) { /* Recebe o JSON e converte pra Objeto */
+
 		Acesso acessoSalvo = acessoService.save(acesso);
-		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK); /* o Response é o TBody! */
+
+		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
 	}
 
 }
