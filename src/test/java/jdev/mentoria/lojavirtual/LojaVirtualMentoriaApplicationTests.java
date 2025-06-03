fb5bba6967d5bf58f427e90d095855426c10186a
Otavio <luizgomesdossantos1970@gmail.com>
2025-06-03 19:38:06 -0300

    Initial configs of flyway and properties.

--- src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
+++ src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,6 +22,7 @@ import jdev.mentoria.lojavirtual.repository.AcessoRepository;
 import junit.framework.TestCase;
 
 @SpringBootTest(classes = LojaVirtualMentoriaApplication.class)
+@Profile("test")
 class LojaVirtualMentoriaApplicationTests extends TestCase {
 
 //	@Autowired
