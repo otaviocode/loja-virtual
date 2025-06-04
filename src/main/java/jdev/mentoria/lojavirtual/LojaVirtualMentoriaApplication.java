55b37baeeecf54371e0ae6be0a53040f3bb02128
Otavio <luizgomesdossantos1970@gmail.com>
2025-06-03 21:20:23 -0300

    For today, alright and all correct and working.

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class LojaVirtualMentoriaApplication {
 
+	/* Hello World testing */
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualMentoriaApplication.class, args);
 	}
